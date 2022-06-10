253622f673f4516c47b9901ef40c91f2eadbd44d
Adam Wiśniewski <45044261+Namlesz@users.noreply.github.com>
2022-06-10 09:06:12 +0200

    feat
    Add details screen
    Change background

---
+++ app/src/main/java/com/example/weatherapp/WeatherDetails.java
@@ -0,0 +1,59 @@
+package com.example.weatherapp;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.text.DecimalFormat;
+
+public class WeatherDetails extends AppCompatActivity {
+    DecimalFormat df = new DecimalFormat("#.##");
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_weather_details);
+
+        TextView info = findViewById(R.id.info);
+
+        Intent intent = getIntent();
+        String response = intent.getStringExtra("Weather");
+        String output = "";
+        try {
+            JSONObject jsonResponse = new JSONObject(response);
+            JSONArray jsonArray = jsonResponse.getJSONArray("weather");
+            JSONObject jsonObjectWeather = jsonArray.getJSONObject(0);
+            String description = jsonObjectWeather.getString("description");
+            JSONObject jsonObjectMain = jsonResponse.getJSONObject("main");
+            double temp = jsonObjectMain.getDouble("temp") - 273.15;
+            double feelsLike = jsonObjectMain.getDouble("feels_like") - 273.15;
+            float pressure = jsonObjectMain.getInt("pressure");
+            int humidity = jsonObjectMain.getInt("humidity");
+            JSONObject jsonObjectWind = jsonResponse.getJSONObject("wind");
+            String wind = jsonObjectWind.getString("speed");
+            JSONObject jsonObjectClouds = jsonResponse.getJSONObject("clouds");
+            String clouds = jsonObjectClouds.getString("all");
+            JSONObject jsonObjectSys = jsonResponse.getJSONObject("sys");
+            String countryName = jsonObjectSys.getString("country");
+            String cityName = jsonResponse.getString("name");
+            output += "Current weather of " + cityName + " (" + countryName + ")"
+                    + "\n Temp: " + df.format(temp) + " °C"
+                    + "\n Feels Like: " + df.format(feelsLike) + " °C"
+                    + "\n Humidity: " + humidity + "%"
+                    + "\n Description: " + description
+                    + "\n Wind Speed: " + wind + "m/s (meters per second)"
+                    + "\n Cloudiness: " + clouds + "%"
+                    + "\n Pressure: " + pressure + " hPa";
+            info.setText(output);
+        } catch (
+                JSONException e) {
+            e.printStackTrace();
+        }
+    }
+}
