a66de75e0af59f5e5962f02e4ca0944612b35ea7
Adam Wiśniewski <45044261+Namlesz@users.noreply.github.com>
2022-06-11 19:49:21 +0200

    finish second screeb

--- app/src/main/java/com/example/weatherapp/WeatherDetails.java
+++ app/src/main/java/com/example/weatherapp/WeatherDetails.java
@@ -13,6 +13,8 @@ import org.json.JSONObject;
 import java.text.DecimalFormat;
 
 public class WeatherDetails extends AppCompatActivity {
+
+    TextView town, feel_temp, wind, temperature, pressure, humidity, cloudiness;
     DecimalFormat df = new DecimalFormat("#.##");
 
     @Override
@@ -20,37 +22,58 @@ public class WeatherDetails extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_weather_details);
 
-        TextView info = findViewById(R.id.temperature);
+        town = findViewById(R.id.town);
+        feel_temp = findViewById(R.id.feel_temp);
+        wind = findViewById(R.id.wind);
+        temperature = findViewById(R.id.temperature);
+        pressure = findViewById(R.id.pressure);
+        humidity = findViewById(R.id.humidity);
+        cloudiness = findViewById(R.id.cloudiness);
 
         Intent intent = getIntent();
         String response = intent.getStringExtra("Weather");
-        String output = "";
         try {
             JSONObject jsonResponse = new JSONObject(response);
-            JSONArray jsonArray = jsonResponse.getJSONArray("weather");
-            JSONObject jsonObjectWeather = jsonArray.getJSONObject(0);
-            String description = jsonObjectWeather.getString("description");
             JSONObject jsonObjectMain = jsonResponse.getJSONObject("main");
+
             double temp = jsonObjectMain.getDouble("temp") - 273.15;
+
             double feelsLike = jsonObjectMain.getDouble("feels_like") - 273.15;
-            float pressure = jsonObjectMain.getInt("pressure");
-            int humidity = jsonObjectMain.getInt("humidity");
-            JSONObject jsonObjectWind = jsonResponse.getJSONObject("wind");
-            String wind = jsonObjectWind.getString("speed");
-            JSONObject jsonObjectClouds = jsonResponse.getJSONObject("clouds");
-            String clouds = jsonObjectClouds.getString("all");
+
+            float pressureVal = jsonObjectMain.getInt("pressure");
+
+            int humidityVal = jsonObjectMain.getInt("humidity");
+
+            String wind_sp = jsonResponse.getJSONObject("wind").getString("speed");
+
+            String clouds = jsonResponse.getJSONObject("clouds").getString("all");
+
             JSONObject jsonObjectSys = jsonResponse.getJSONObject("sys");
             String countryName = jsonObjectSys.getString("country");
             String cityName = jsonResponse.getString("name");
-            output += "Current weather of " + cityName + " (" + countryName + ")"
-                    + "\n Temp: " + df.format(temp) + " °C"
-                    + "\n Feels Like: " + df.format(feelsLike) + " °C"
-                    + "\n Humidity: " + humidity + "%"
-                    + "\n Description: " + description
-                    + "\n Wind Speed: " + wind + "m/s (meters per second)"
-                    + "\n Cloudiness: " + clouds + "%"
-                    + "\n Pressure: " + pressure + " hPa";
-            info.setText(output);
+
+            String _town = cityName + " (" + countryName + ")";
+            town.setText(_town);
+
+            String _feel_temp = "Odczuwalna temperatura:\n" +
+                    df.format(feelsLike) +" °C";
+            feel_temp.setText(_feel_temp);
+
+            String _wind = "Wiatr:\n" + wind_sp + "m/s";
+            wind.setText(_wind);
+
+            String _temperature = "Temperatura:\n" + df.format(temp) + " °C";
+            temperature.setText(_temperature);
+
+            String _pressure = "Ciśnienie:\n" + pressureVal + " hPa";
+            pressure.setText(_pressure);
+
+            String _humidity = "Wilgotność:\n" + humidityVal + "%";
+            humidity.setText(_humidity);
+
+            String _cloudiness = "Zachmurzenie:\n" + clouds + "%";
+            cloudiness.setText(_cloudiness);
+
         } catch (
                 JSONException e) {
             e.printStackTrace();
